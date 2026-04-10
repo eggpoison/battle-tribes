70620a4db0e31b39b7b60ac3ea17a1852499fb88
eggpoison <edwrd.magee@gmail.com>
2026-04-10 16:29:24 +1000

    useless change

--- client/src/game/client.ts
+++ client/src/game/client.ts
@@ -171,7 +171,7 @@ export function establishNetworkConnection(username: string, tribeType: TribeTyp
    }
    
    // @SQUEAM
-   socket = new WebSocket(`ws://10.0.0.10:${Settings.SERVER_PORT}`);
+   socket = new WebSocket(`ws://10.0.0.23:${Settings.SERVER_PORT}`);
    // socket = new WebSocket(`ws://127.0.0.1:${Settings.SERVER_PORT}`);
    // socket = new WebSocket(`ws://localhost:${Settings.SERVER_PORT}`);
    socket.binaryType = "arraybuffer";
